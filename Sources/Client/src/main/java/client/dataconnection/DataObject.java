98b22421e38775099a9eacc7c23aab058b761723
Oskar Koiner <okoiner@student.tgm.ac.at>
2018-05-16 10:09:10 +0200

    add funcitons to dataobject

--- Sources/Client/src/main/java/client/dataconnection/DataObject.java
+++ Sources/Client/src/main/java/client/dataconnection/DataObject.java
@@ -1,5 +1,14 @@
 package client.dataconnection;
 
 public interface DataObject {
+    void register(String username, String password);
+    void login(String username, String password);
+    void kommentier(String eventName);
+    void kommentarLöschen(String eventName, int kommentarID);
+    void einladen(String eventName, String benutzerName);
+    void entladent(String eventName, String benutzerName);
+    void terminFestlegen(String eventName, int terminID);
+    void terminAbstimmen(String eventName, int terminID);
+    
 
 }
