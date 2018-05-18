32fb34e55dea7ee56296e1ccfbefeecf157c0605
mseemann-tgm <mseemann@student.tgm.ac.at>
2018-05-18 12:12:59 +0200

    insert fields login panel added

--- Sources/Client/src/main/java/client/view/LoginPanel.java
+++ Sources/Client/src/main/java/client/view/LoginPanel.java
@@ -1,4 +1,29 @@
 package client.view;
 
-public class LoginPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginPanel extends JPanel {
+    private JLabel benutzerText;
+    private JLabel passwortText;
+    private JTextField benutzerEingabe;
+    private JTextField passwortEingabe;
+    private JButton eingabeButton;
+    public LoginPanel(){
+        this.setLayout(new GridLayout(3, 2));
+        this.benutzerText = new JLabel("Benutzer:");
+        this.passwortText = new JLabel("Passwort:");
+        this.benutzerEingabe = new JTextField();
+        this.passwortEingabe = new JTextField();
+        this.eingabeButton = new JButton("Eingabe");
+
+        this.add(this.benutzerText);
+        this.add(this.benutzerEingabe);
+        this.add(this.passwortText);
+        this.add(this.passwortEingabe);
+        this.add(this.eingabeButton);
+
+
+
+    }
 }
--- Sources/Client/src/main/java/client/view/View.java
+++ Sources/Client/src/main/java/client/view/View.java
@@ -1,5 +1,44 @@
 package client.view;
 
-public class View {
+import client.controller.Controller;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Die Klasse View erzeugt das Frame.
+ *
+ * @author SEEMANN Manuel
+ * @version 2018-05-17
+ */
+
+public class View extends JFrame{
+    private Controller c;
+    private JPanel p;
+
+
+    /**
+     * Der Konstruktor erzeugt das Frame.
+     *
+     * @param c Controller
+     */
+
+    public View(Controller c){
+
+        this.setTitle("Terminreservierungssystem");
+        this.setSize(1000, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setLayout(new FlowLayout ());
+
+        this.c = c;
+        this.p = new LoginPanel();
+
+        this.add(this.p);
+
+
+        this.setVisible(true);
+    }
 
 }
