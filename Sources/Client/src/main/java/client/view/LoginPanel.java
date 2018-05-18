1a4f747941069bd91d70ad8e4014aa3d936f3537
mseemann-tgm <mseemann@student.tgm.ac.at>
2018-05-18 13:11:59 +0200

    jbutton registrieung

--- Sources/Client/src/main/java/client/view/LoginPanel.java
+++ Sources/Client/src/main/java/client/view/LoginPanel.java
@@ -9,6 +9,8 @@ public class LoginPanel extends JPanel {
     private JTextField benutzerEingabe;
     private JTextField passwortEingabe;
     private JButton eingabeButton;
+    private JButton registrierungButton;
+
     public LoginPanel(){
         this.setLayout(new GridLayout(3, 2));
         this.benutzerText = new JLabel("Benutzer:");
@@ -16,11 +18,13 @@ public class LoginPanel extends JPanel {
         this.benutzerEingabe = new JTextField();
         this.passwortEingabe = new JTextField();
         this.eingabeButton = new JButton("Eingabe");
+        this.registrierungButton = new JButton("Registrieren");
 
         this.add(this.benutzerText);
         this.add(this.benutzerEingabe);
         this.add(this.passwortText);
         this.add(this.passwortEingabe);
+        this.add(this.registrierungButton);
         this.add(this.eingabeButton);
 
 
