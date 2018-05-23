44669b7103e449e8d6d64d563472c6956c87a9e5
mseemann-tgm <mseemann@student.tgm.ac.at>
2018-05-23 14:57:43 +0200

    benutzersuchen scrollable list new panel layout

--- Sources/Client/src/main/java/client/view/BenutzerSuchenPanel.java
+++ Sources/Client/src/main/java/client/view/BenutzerSuchenPanel.java
@@ -2,21 +2,42 @@ package client.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class BenutzerSuchenPanel extends JPanel {
     private JLabel benutzerEingabeText;
     private  JTextField benutzerEingabe;
     private JButton benutzerEingabeButton;
+    private JList benutzerListe;
+    private JScrollPane benutzerListeScroll;
+    private JPanel eingabePanel;
 
     public BenutzerSuchenPanel(){
-        this.setLayout(new GridLayout(0, 3));
+        String [] data = {"Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin","Alex","Armin"};
+        this.setLayout(new GridLayout(2,3));
         this.benutzerEingabeText = new JLabel("Benutzername: ");
         this.benutzerEingabe = new JTextField();
         this.benutzerEingabeButton = new JButton("Suchen");
+        this.benutzerListe = new JList(data);
+        this.eingabePanel = new JPanel();
+        this.eingabePanel.setLayout(new FlowLayout());
+        this.benutzerEingabe.setPreferredSize(new Dimension(90,25));
 
-        this.add(this.benutzerEingabeText);
-        this.add(this.benutzerEingabe);
-        this.add(this.benutzerEingabeButton);
+
+        this.benutzerListe.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        this.benutzerListe.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        this.benutzerListe.setVisibleRowCount(-1);
+
+        this.benutzerListeScroll = new JScrollPane(this.benutzerListe);
+        this.benutzerListeScroll.setPreferredSize(new Dimension(250, 80));
+
+
+        this.add(this.eingabePanel);
+        this.eingabePanel.add(this.benutzerEingabeText);
+        this.eingabePanel.add(this.benutzerEingabe);
+        this.eingabePanel.add(this.benutzerEingabeButton);
+        this.add(this.benutzerListeScroll);
+
 
     }
 }
