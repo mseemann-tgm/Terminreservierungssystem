ddc6232f752b72327f17a6d690e3871a5e7afd94
Oskar Koiner <okoiner@student.tgm.ac.at>
2018-05-28 11:56:42 +0200

    Add JUnit to Data

---
+++ Sources/Data/src/test/java/data/DataTests.java
@@ -0,0 +1,12 @@
+package data;
+
+import junit.framework.TestCase;
+
+public class DataTests extends TestCase{
+    protected void setUp() {
+
+    }
+    public void testTest(){
+        assertTrue(true);
+    }
+}
