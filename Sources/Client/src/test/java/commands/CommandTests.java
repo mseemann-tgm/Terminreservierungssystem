8be65d36fbc4381d117a2d8136980b8efe5dbcf6
Oskar Koiner <okoiner@student.tgm.ac.at>
2018-05-30 11:51:24 +0200

    Add junit to client

---
+++ Sources/Client/src/test/java/commands/CommandTests.java
@@ -0,0 +1,29 @@
+package commands;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+import java.util.List;
+
+public class CommandTests{
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testTest(){
+    }
+
+}
+
+
+
