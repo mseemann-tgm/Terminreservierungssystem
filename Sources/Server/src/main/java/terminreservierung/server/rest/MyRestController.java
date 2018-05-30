3de40777488bc9669d9c6d521d90fa49927aaf31
sappel-tgm <sappel@student.tgm.ac.at>
2018-05-30 15:59:52 +0200

    Upload Rest Commands

--- Sources/Server/src/main/java/terminreservierung/server/rest/MyRestController.java
+++ Sources/Server/src/main/java/terminreservierung/server/rest/MyRestController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @RestController
 public class MyRestController {
 
@@ -35,4 +38,78 @@ public class MyRestController {
         System.out.println("Ampunt:"+w.getI());
         counter.addAndGet(w.getI());
     }
+
+    @RequestMapping("/User/login")
+    public void login(@RequestParam(value = "name", defaultValue = "World") String username) {
+        System.out.println("login"+ username);
+
+    }
+
+    @RequestMapping("/User/register")
+    public void register(@RequestParam(value = "name", defaultValue = "World") String userkey) {
+        System.out.println("Register" + userkey);
+    }
+
+    @RequestMapping("/Termin/choose")
+    public void chooseTermin(String userkey){
+        System.out.println("chooseTermin by "+userkey);
+    }
+
+    @RequestMapping("/User/search")
+    public void searchUser(String userkey){
+        System.out.println("searchUser by "+userkey);
+    }
+
+    @RequestMapping("/Events/show")
+    public void showEvents(String userkey){
+        System.out.println("showEvents by "+userkey);
+    }
+
+    @RequestMapping("/Comment/delete")
+    public void deleteUser(String userkey){
+        System.out.println("deleteComment by "+userkey);
+    }
+
+    @RequestMapping("/User/invite")
+    public void inviteUser(String userkey){
+        System.out.println("inviteUser by "+userkey);
+    }
+
+    @RequestMapping("/User/invite/delete")
+    public void deleteInvite(String userkey){
+        System.out.println("deleteInvite by "+userkey);
+    }
+
+    @RequestMapping("/Termin/set")
+    public void setTermin(String userkey){
+        System.out.println("setTermin by"+userkey);
+    }
+
+    @RequestMapping("/Event/create")
+    public void createEvent(String userkey){
+        System.out.println("createEvent by"+userkey);
+    }
+
+    @RequestMapping("/Event/change")
+    public void changeEvent(String userkey){
+        System.out.println("changeEvent by"+userkey);
+    }
+        /*System.out.println("register");
+        String search = username;
+        boolean alreadyRegistered = false;
+        for(String str: registeredUsers) {
+            if (str.trim().toLowerCase().contains(search))
+                alreadyRegistered = true;
+        }
+
+
+        if(alreadyRegistered==false){
+            registeredUsers.add(username);
+            System.out.println("Registering of "+username+" was successful");
+        }else{
+            System.out.println("Username "+username+" alread taken");
+        }
+        */
+
+
 }
