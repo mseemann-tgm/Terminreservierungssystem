c973934a6f0d89c3709c7751ec718464c4e7e73a
Oskar Koiner <okoiner@student.tgm.ac.at>
2018-05-30 16:03:28 +0200

    Add login

--- Sources/Server/src/main/java/terminreservierung/server/rest/MyRestController.java
+++ Sources/Server/src/main/java/terminreservierung/server/rest/MyRestController.java
@@ -1,7 +1,11 @@
 package terminreservierung.server.rest;
 
+import terminreservierung.server.rest.data.User;
+
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.SecureRandom;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -35,4 +39,29 @@ public class MyRestController {
         System.out.println("Ampunt:"+w.getI());
         counter.addAndGet(w.getI());
     }
+
+
+    @PostMapping("/login")
+    public String login(@RequestBody User u, HttpServletRequest request){
+        final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+        int len = 64;
+        StringBuilder sb = new StringBuilder( len );
+        for( int i = 0; i < len; i++ )
+            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+        String sessionCert  = sb.toString();
+
+        SessionCertificate c = new SessionCertificate(request.getRemoteAddr(),u.getName());
+
+        certificates.put(sessionCert,c);
+
+        return sessionCert;
+    }
 }
+
+
+
+
+
+
+
---
+++ Sources/Server/src/main/java/terminreservierung/server/rest/data/User.java
@@ -0,0 +1,22 @@
+package terminreservierung.server.rest.data;
+
+public class User {
+    private String name;
+    private String passwort;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+}
