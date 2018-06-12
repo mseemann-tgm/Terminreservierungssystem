a6dd931e0f525ce394f011719acb27d1109deb22
mseemann-tgm <mseemann@student.tgm.ac.at>
2018-06-12 17:54:17 +0200

    kommentare für main

--- Sources/Client/src/main/java/client/Main.java
+++ Sources/Client/src/main/java/client/Main.java
@@ -2,6 +2,11 @@ package client;
 
 import client.controller.Controller;
 
+/**
+ * Main erstellt den Controller
+ * @author Koiner Oskar
+ * @version 2018-04-25
+ */
 public class Main {
     public static void main(String[] args){
         new Controller();
