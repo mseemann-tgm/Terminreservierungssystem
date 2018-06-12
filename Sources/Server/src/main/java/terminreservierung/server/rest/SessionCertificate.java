499a4dd67832b41fe26fd917b0333fa0692ae10a
sappel-tgm <sappel@student.tgm.ac.at>
2018-06-12 10:48:00 +0200

    Kommentare Server SessionCertificate

--- Sources/Server/src/main/java/terminreservierung/server/rest/SessionCertificate.java
+++ Sources/Server/src/main/java/terminreservierung/server/rest/SessionCertificate.java
@@ -1,12 +1,23 @@
 package terminreservierung.server.rest;
-
+/*
+ * SessionCertificate to identify User per Session
+ *
+ */
 public class SessionCertificate {
     private String remoteAddress;
     private String username;
+    /*
+     * get address and username and save it
+     */
     public SessionCertificate(String remoteAddress, String username){
         this.remoteAddress=remoteAddress;
         this.username=username;
     }
+    /*
+     * check if address and username is equal to saved ones
+     *
+     * return true if both params are equal
+     */
     public boolean isValid(String remoteAddress, String username){
         return remoteAddress==this.remoteAddress&&username==this.username;
     }
