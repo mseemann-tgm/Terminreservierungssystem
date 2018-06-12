f000a4c19f21c16a9caecf0407dc7dc550ca753b
sappel-tgm <sappel@student.tgm.ac.at>
2018-06-12 10:44:00 +0200

    Kommentare Server Main

--- Sources/Server/src/main/java/terminreservierung/server/Main.java
+++ Sources/Server/src/main/java/terminreservierung/server/Main.java
@@ -3,12 +3,20 @@ package terminreservierung.server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/* Main.java
+ *
+ * Spring Application runt die Main Klasse
+ *
+ *
+ */
 @SpringBootApplication
 public class Main implements CommandLineRunner{
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+    /*
+     * On SpringApplication.run, print that it runs
+     */
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Running");
